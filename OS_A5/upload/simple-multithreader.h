a12a7d37823b2b7f9958358de08fcb6851526c48
Tanish Verma <astronomicaltanish@gmail.com>
2023-11-17 13:45:43 +0530

    ye

--- OS_A5/upload/simple-multithreader.h
+++ OS_A5/upload/simple-multithreader.h
@@ -20,10 +20,30 @@ typedef struct{
 
 }thread_args;
 
+typedef struct{
+  int low1;
+  int high1;
+  int low2;
+  int high2;
+  std::function<void(int,int)> lambda;
+}thread_args_12;
+
+
 void *thread_func(void* ptr){
   thread_args * t = ((thread_args *) ptr); 
   for (int p = t->low; p < t->high; p++){
     t->lambda(p);
+  }
+  return NULL;
+}
+
+void *thread_func_12(void* ptr){
+  thread_args_12 * t = ((thread_args_12 *) ptr); 
+  for (int p = t->low1; p < t->high1; p++){
+    for (int j = t->low2; p < t->high2; j++){
+      printf("huh");
+      t->lambda(p,j);
+    }
   }
   return NULL;
 }
@@ -48,7 +68,28 @@ void parallel_for(int low, int high, std::function<void(int)> &&lambda, int numT
 }
 
 void parallel_for(int low1, int high1,  int low2, int high2, std::function<void(int, int)>  &&lambda, int numThreads){
-  
+  pthread_t tid[numThreads];
+  thread_args_12 args[numThreads];
+  int size = high2 - low2;
+  int chunk1 = (high1 - low1) / numThreads;
+  // int chunk2 = size;
+  int rem1 = (high1 - low1) % numThreads;
+  // int rem2 = (high2 - low2) % numThreads;
+  for (int i=0; i<numThreads; i++){
+    args[i].low1 = i*chunk1;
+    args[i].high1 = args[i].low1 + chunk1;
+    args[i].low2 = low2;
+    args[i].high2 = high2;
+    if (i == numThreads - 1){
+      args[i].high1 += rem1;
+      // args[i].high2 += rem2;
+    }
+    args[i].lambda = lambda;
+    pthread_create(&tid[i], NULL, thread_func_12, (&args[i]));
+  }
+  for (int i=0; i<numThreads; i++){
+    pthread_join(tid[i], NULL);
+  }  
 }
 
 int main(int argc, char **argv) {
